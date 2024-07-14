3683fe350cdb3e15307fb1bed5502487c68627fe
Tarek Farouq <89213763+TarekFarouqq@users.noreply.github.com>
2024-07-14 17:10:59 +0300

    Add files via upload

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Tarek Ahmed on 14/07/2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
